047d9ea89c24a1c7e11534c2cbcdbb1a68e1d8b4
daniitoro29 <daniitoro29@gmail.com>
2025-02-17 20:18:21 -0500

    Added: initial routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
-import { Home } from "./pages/index";
+import { Routes, Route } from "react-router-dom";
+
+import { Home, Admin } from "./pages/index";
+import { Header } from "./components/index";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <Home />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<Admin />} />
+        {/*         <Route path="/recomendaciones" element={<RecomendacionesPage />} />
+        <Route path="/explora" element={<ExploraPage />} /> 
+        <Route path="*" element={<NotFoundPage />} />*/}
+      </Routes>
     </>
   );
 }
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,9 +1,7 @@
-import { Header } from "../../components/index";
-
 export const Home = () => {
   return (
     <>
-      <Header />
+      Holi
     </>
   );
 };
