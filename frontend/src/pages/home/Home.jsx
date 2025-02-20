2b2243b29134166a87c4169288fa9dfe84ce8a72
daniela1421 <daniela.ducuara1817@gmail.com>
2025-02-19 20:13:19 -0500

    Feat: structure styles and functionality of the home

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,7 +1,14 @@
+import { Box, Typography } from "@mui/material";
+import { ImageSlider } from "../../components/imageSlider/ImageSlider";
+import { Recommendations } from "../../components/recommendations/recommendations";
+import { Explore } from "../../components/explore/Explore";
+
 export const Home = () => {
   return (
-    <>
-      Holi
-    </>
+    <Box sx={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", mb: 5}}>
+      <ImageSlider/>
+      <Recommendations/>
+      <Explore />
+    </Box>
   );
 };
