45c3dc36752caa01fae85773b5915addd3baf216
Daniela Toro <97687455+daniitoro29@users.noreply.github.com>
2025-02-24 18:15:04 -0500

    Revert "Feature/body"

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,14 +1,7 @@
-import { Box, Typography } from "@mui/material";
-import { ImageSlider } from "../../components/imageSlider/ImageSlider";
-import { Recommendations } from "../../components/recommendations/recommendations";
-import { Explore } from "../../components/explore/Explore";
-
 export const Home = () => {
   return (
-    <Box sx={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", mb: 5}}>
-      <ImageSlider/>
-      <Recommendations/>
-      <Explore />
-    </Box>
+    <>
+      Holi
+    </>
   );
 };
