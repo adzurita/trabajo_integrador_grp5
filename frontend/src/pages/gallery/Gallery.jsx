9f6bc24b1e81f0c49614a3738f4085f3c9bb050e
daniela1421 <daniela.ducuara1817@gmail.com>
2025-03-04 23:06:48 -0500

    Add the login component and create the context to handle the global state of the user

--- frontend/src/pages/gallery/Gallery.jsx
+++ frontend/src/pages/gallery/Gallery.jsx
@@ -15,21 +15,15 @@ export const Gallery = () => {
 
     return (
         <Box sx={{ width: "90%", height: "90vh", overflowY: "auto", margin: 'auto' }}>
-            {/* Header */}
             <Box sx={{ width: "100%", py: 2, px: 3, bgcolor: "white", position: "fixed", top: 0, left: 0, zIndex: 10, boxShadow: "0 2px 4px rgba(0,0,0,0.1)" }}>
                 <Typography variant="h6" fontWeight="bold">Galería de imágenes</Typography>
                 <Typography variant="body2" onClick={() => navigate(-1)} sx={{ cursor: "pointer", color: "blue" }}>← Volver</Typography>
             </Box>
-
-            {/* Galería */}
             <Box sx={{ pt: 10, px: 2 }}>
                 <Grid container spacing={2}>
-                    {/* Imagen Grande */}
                     <Grid item xs={12}>
                         <img src={dummyImages[0]} alt="Imagen principal" style={{ width: "100%", height: 500, objectFit: "cover", borderRadius: "8px" }} />
                     </Grid>
-
-                    {/* Dos imágenes a mitad de ancho */}
                     <Grid item xs={6}>
                         <img src={dummyImages[1]} alt="Imagen secundaria" style={{ width: "100%", height: 300, objectFit: "cover", borderRadius: "8px" }} />
                     </Grid>
